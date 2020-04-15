ae3d51e616cdc8c33c9e60eeab5bf20a8d87b3fe
GsminGitHub <1500969875@qq.com>
2020-04-15 09:29:52 +0800

    报表模块

--- lib/page/gfz/business_page.dart
+++ lib/page/gfz/business_page.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:wanma_jituan/common/utils/screen_util.dart';
+import 'dart:convert';
+
+import 'package:wanma_jituan/common/json/json_string.dart';
+import 'package:wanma_jituan/widget/grid_item.dart';
 
 class BusinessPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('报表'),
-      ),
-    );
+        child: GridView(
+          padding: const EdgeInsets.all(20.0),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 100,
+              crossAxisSpacing: 10.0
+          ),
+          children: _getItem(),
+        ),
+      );
+  }
+
+  _getItem() {
+    final items = <Widget>[];
+    var data = (json.decode(JsonString.gfzReportFormData))['result'];
+    for(int i = 0;i<data.length;i++) {
+      var item = GridItemWidget(
+        text: data[i]['title'],
+        functionName: data[i]['functionName'],
+        mid: data[i]['id'].toString(),
+      );
+      items.add(item);
+    }
+    return items;
   }
 }
